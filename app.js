170c2f423a2e108cc3bf4ac1565add00dcd6fbc7
YKASSABI <youssef@devmyk.com>
2019-04-01 01:24:35 +0100

    linked photo and changed the outlook and added err page

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
-
+const path = require('path');
+const createError = require('http-errors');
 const app = express();
-const PORT = 3001;
+const PORT = 4000;
 
 app.set('view engine' , 'pug');
-app.use(express.static("public"));
+app.set('views', path.join(__dirname, "./views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const route = require('./routes');
 
-app.use(route);
+app.use("/", route);
 
+app.use((req,res,next)=> next(createError(404, 'File not Found')))
 
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    res.locals.status = err.status || 500;
+    res.locals.error = err;
+    console.log(`Sorry ! the requested url  \" ${req.url} \" does\'t exist!`)
+    return res.render('err')})
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,53 @@ const express = require('express');
 const fs = require('fs');
 const route = express.Router();
 
-
+// preparing data from jason file
 const aa = fs.readFileSync('./data/data.json','utf8', (d)=>{
     });
 const dataObj = JSON.parse(aa);
 
 
-
-route.get('/',(req, res,next)=>{
-    res.render("index",{ data: dataObj.projects});
-    next()
+route.get('/favicon.ico', (req, res, next) =>{
+     res.sendStatus(204);
 })
 
+
+// Routing Homepage
+route.get('/',(req, res,next)=>{
+    res.render("index",{ data: dataObj.projects});
+    // next()
+})
+
+// Routing about
 route.get('/about',(req, res)=>{
     res.render("about");
 })
 
+
+
+// Routing  Projects URL
+route.get('/project', (req,res, next)=>{
+        res.redirect("/")
+})
+
 route.get('/project/:id', (req,res)=>{
-    if(isNaN(Number(req.params.id))){
-        res.send("is naaaan")
-    }else if((Number(req.params.id)) > dataObj.projects.length){
-        res.send("it s not in ")
+
+    let theID = req.params.id;
+    let nextProject = Number(theID) === 6 ? 0: Number(theID)+1
+    
+    if(isNaN(Number(theID))){
+        res.redirect("/")
+    }else if((Number(theID)) > dataObj.projects.length){
+        res.redirect("/")
     }else{
-    res.render(`project`, {curProject : dataObj.projects[req.params.id] });
+    res.render(`project`, {
+        curProject : dataObj.projects[theID], 
+        nextProject : nextProject 
+        });
     }
 })
 
 
-route.use((err,req,res,next)=>{
-    console.log('error here')
-    res.status(500).send(`Alert !! : ${err.stack}`)
-})
 
 
 
